dccef39b2a8f1b609021ecb81429977116eeeebe
Luke Masterman <luke.masterman@bp.com>
2023-06-04 13:14:10 +0100

    First stage jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,52 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build API container') {
+            steps {
+                sh 'docker build . -t masterman/movie-api'
+            }
+        }
+        stage ('Run API container') {
+            steps {
+                sh ' docker run -p 3000:3000 --network bridge --name api-container --rm -d masterman/movie-api'
+            }
+        }
+        stage('Build Test container') {
+            steps {
+                sh 'docker build tests -t masterman/movie-api-tests'
+            }
+        }
+        stage('Execute tests') {
+            steps {
+                sh 'docker volume create test-reports'
+                sh 'docker run -e PORT=3000 -e BASE_URI=172.17.0.2 --network bridge -v test-reports:/usr/src/app --rm masterman/movie-api-tests'
+            }
+        }
+        stage('Retrieve reports') {
+            steps {
+                sh 'docker container create --name report-container -v test-reports:/root alpine'
+                sh 'docker cp report-container:/root/reports/. ${WORKSPACE}/reports'
+            }
+        }
+        stage('Cleanup') {
+            steps {
+                sh 'docker kill api-container'
+                sh 'docker rm report-container'
+                sh 'docker volume rm test-reports'
+            }
+        }
+        stage('Publish Test Results') {
+            steps {
+                junit skipPublishingChecks: true, testResults: 'reports/jest-junit.xml'
+                publishHTML target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: true,
+                    reportDir: 'reports',
+                    reportFiles: 'test-report.html',
+                    reportName: 'Test Results'
+                ]
+            }
+        }
+    }
+}
--- tests/__tests__/movie-tests.js
+++ tests/__tests__/movie-tests.js
@@ -3,11 +3,9 @@ const request = require("supertest")
 const movies = require("../test_data/movies.json")
 
 let baseUri = process.env.BASE_URI ?? "http://localhost"
-let port = process.env.PORT
+let port = process.env.PORT ?? 3000
 
 let testUri = port ? `${baseUri}:${port}` : baseUri
-
-console.log(`BaseUri: ${baseUri}, Port: ${port}, TestUri:${testUri}`)
 
 module.exports = {
 	testEnvironment: "node",
@@ -15,48 +13,64 @@ module.exports = {
 
 describe("Movie API endpoint tests", () => {
 
-    test("GET /api/movies", () => {
+    test("GET /api/movies", (done) => {
         request(testUri)
             .get('/api/movies')
-            .end((err, res) => {
-                expect(res.statusCode).toBe(200)
+            .expect(200)
+            .expect((res) => {
                 expect(res.body.length).toBeGreaterThan(1)
                 expect(res.body).toEqual(movies)
             })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            })
     })
 
-    test("GET /api/movies/title/:title", () => {
+    test("GET /api/movies/title/:title", (done) => {
         let title = "Lord of the Rings: The Two Towers"
         request(testUri)
             .get('/api/movies/title/' + encodeURI(title))
-            .end((err, res) => {
-                expect(res.statusCode).toBe(200)
+            .expect(200)
+            .expect((res) => {
                 expect(res.body.title).toBe(title)
                 expect(res.body.rating).toBe(10)
                 expect(res.body.certificate).toBe("12")
                 expect(res.body.release_date).toBe(2002)
                 expect(res.body.directed_by).toBe("Peter Jackson")
             })
-    })
-
-    test("GET /api/movies/director/:director", () => {
-        let director = "Peter Jackson"
-        request(testUri)
-            .get("/api/movies/director/" + encodeURI(director))
             .end((err, res) => {
-                expect(res.statusCode).toBe(200)
-                expect(res.body.length).toBe(3)
+                if (err) return done(err);
+                return done();
             })
     })
 
-    test("GET /api/movies/rating/:method/:rating", () => {
+    test("GET /api/movies/director/:director", (done) => {
+        let director = "Peter Jackson"
+        request(testUri)
+            .get("/api/movies/director/" + encodeURI(director))
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(3)
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
+            })
+    })
+
+    test("GET /api/movies/rating/:method/:rating", (done) => {
         let method = "moreThanOrEqual"
         let rating = 8
         request(testUri)
             .get(`/api/movies/rating/${method}/${rating}`)
-            .end((err, res) => {
-                expect(res.statusCode).toBe(200)
+            .expect(200)
+            .expect((res) => {
                 expect(res.body.length).toBe(7)
+            })
+            .end((err, res) => {
+                if (err) return done(err);
+                return done();
             })
     })
 })
